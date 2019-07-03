4731819882cf1aa2c6ce90cb79ce873afea3ed23
Emerson Costa <vmaster204@gmail.com>
2019-07-03 17:52:43 -0400

    Ultima Atualizacao
    Projeto Final

--- Segundo Trabalho/src/main/Main2.java
+++ Segundo Trabalho/src/main/Main2.java
@@ -2,6 +2,7 @@
 package main;
 
 import classes.Banco;
+import java.util.Scanner;
 
 
 
@@ -10,19 +11,88 @@ public class Main2 {
     
     public static void main(String[] args) {
         
-        /*Testes Realizados Durante o Desenvolvimento*/
-        Banco banco = new Banco();
-        banco.cadastrarAgencia("Banco do Brasil", "2224","Brasil", "Campo Grande-MS","Rua Afonso Pena","Centro" , "79050-060",10);
-        banco.cadastrarAgencia("Banco Santander", "1124","Brasil", "Campo Grande-MS","Rua Afonso Pena","Centro" , "79050-060",20);
-        banco.abrirConta("C","2224",100,"Juvenal Botelho Pinto","01/09/1940","000","TRADICIONAL","Brasil","Ponta Porà","Glauce Rocha","Julia Cardinal","78090-234",24);
-        banco.abrirConta("F","1124",100,"Juvenal Botelho Pinto","01/09/1940","000","TRADICIONAL","","","","","",0);
-        banco.abrirConta("C","1124",200,"Jacinto Cabeção","01/09/1940","111","TRADICIONAL","Brasil","Ponta Porà","Glauce Rocha","Julia Cardinal","78090-234",24);
-        banco.depositar("2224", "00001", 50);
-        banco.transferencia("2224","00001","1124","00001", 50);
-        banco.viradaMes();
-        banco.gerarRelatorio();
-        banco.fecharConta("1124","00002");
-        System.out.println(banco.getAgencias());  
-    }
-    
+        Scanner ler = new Scanner(System.in);
+        Banco   banco = new Banco();
+       
+        int op = 0;
+        while(op != 10){
+            
+           System.out.print("Informe a Opcao: ");
+           op  = ler.nextInt(); ler.nextLine();
+           
+           switch(op){
+           
+               case 1: //Virada do Mês
+                   
+                     banco.viradaMes();
+                     
+                   break;
+               
+               case 2://Cadastrar Agencia
+                   
+                   /*Dados Pré-Cadastrados*/
+                     banco.cadastrarAgencia("Banco do Brasil", "2224","Brasil", "Campo Grande-MS","Rua Afonso Pena","Centro" , "79050-060",10);
+                     banco.cadastrarAgencia("Banco Santander", "1124","Brasil", "Campo Grande-MS","Rua Afonso Pena","Centro" , "79050-060",20);
+                
+                   break;
+                   
+               case 3://Abertura de Conta
+                   
+                     /*Dados Pre Cadastrados*/
+                     banco.abrirConta("C","2224",100,"Juvenal Botelho ","01/09/1940","000","TRADICIONAL","Brasil","Ponta Porà","Glauce Rocha","Julia Cardinal","78090-234",24);
+                     banco.abrirConta("F","1124",100,"Juvenal Botelho ","01/09/1940","000","TRADICIONAL","","","","","",0);
+                     banco.abrirConta("C","1124",200,"Jacinto Chagas","01/09/1940","111","TRADICIONAL","Brasil","Ponta Porà","Glauce Rocha","Julia Cardinal","78090-234",24);
+                     banco.abrirConta("C","1124",200,"Aracelia Arara","01/09/1940","111","PREMIUM","Brasil","Ponta Porà","Glauce Rocha","Julia Cardinal","78090-234",24);
+
+                   break;
+                   
+               case 4://Saque
+                   
+                   /*Dados Pré-Cadastrados*/
+                   banco.saque("2224", "00001", 20);
+              
+                   break;
+                   
+               case 5://Depósito em Conta
+                   
+                   /*Dados Pré-Cadastrados*/
+                   banco.depositar("2224", "00001", 50);
+              
+                   break;
+                   
+               case 6: //Transferência
+                   
+                   /*Dados Pre-Cadrastrados*/
+                   banco.transferencia("2224","00001","1124","00001", 50);
+               
+                   break;
+                   
+               case 7: //Gerar Extratos
+                   
+                   /*Dados Pre-Cadastrados*/
+                   System.out.println(banco.gerandoExtrato("2224","00001"));
+                   
+                   break;
+                   
+               case 8: //Gerar Relatórios
+                   
+                   banco.gerarRelatorio();
+                
+                   break;
+                   
+               case 9:
+                   
+                   banco.fecharConta("2224","00001");
+                   break;
+                           
+               default:
+                   
+                   if(op != 0 && op != 10){
+                        System.out.println("Entrada inválida");
+                   }
+                   
+                   break;    
+           } 
+        }
+    } 
 }
